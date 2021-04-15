3fee9c73fae1ebeed456ef560e9fce2992b2eea2
Joseph Henry <joseph.henry@zerotier.com>
2021-04-15 14:30:12 -0700

    Bump

--- style/slate/javascripts/all_nosearch.js
+++ style/slate/javascripts/all_nosearch.js
@@ -9,7 +9,7 @@ function adjustLanguageSelectorWidth() {
 }
 
 $(function() {
-  loadToc($('#toc'), '.toc-link', '.toc-list-h2', '.toc-list-h3' 10);
+  loadToc($('#toc'), '.toc-link', '.toc-list-h2', 10);
   setupLanguages($('body').data('languages'));
   $('.content').imagesLoaded( function() {
     window.recacheHeights();
