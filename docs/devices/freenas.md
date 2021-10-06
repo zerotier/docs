ff5b4633fa3f98f90491f320b9e407de7bb5c328
Joseph Henry <joseph.henry@zerotier.com>
2021-10-05 18:06:31 -0700

    Add Western Digital and FreeNAS article stubs

---
+++ docs/devices/freenas.md
@@ -0,0 +1,17 @@
+---
+title: FreeNAS
+
+includes:
+  - errors
+
+search: true
+
+code_clipboard: true
+custom_edit_url: null
+---
+
+FreeNAS
+=====
+
+ZeroTier comes pre-installed on FreeNAS as of FreeNAS-11.0-RC2
+
--- sidebars.js
+++ sidebars.js
@@ -108,7 +108,7 @@ module.exports = {
     {
       type: "category",
       label: "Supported Devices",
-      items: ["devices/synology", "devices/ubiquiti", "devices/qnap", "devices/asustor"],
+      items: ["devices/synology", "devices/ubiquiti", "devices/qnap", "devices/asustor", "devices/freenas", "devices/western-digital"],
     },
     {
       type: "category",
