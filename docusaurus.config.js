37d23c1829377199851ce5bde91bbda8b2984f77
travisladuke <travisladuke@gmail.com>
2023-09-14 13:11:10 -0700

    remove algolia config. add docusaurus-lunr-search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,41 +11,13 @@ module.exports = {
   favicon: "img/favicon-32x32.png",
   organizationName: "zerotier", // Usually your GitHub org/user name.
   projectName: "docs", // Usually your repo name.
-  plugins: [path.resolve(__dirname, "matomo-plugin")],
+  plugins: [path.resolve(__dirname, "matomo-plugin"), require.resolve('docusaurus-lunr-search')],
   markdown: {
     mermaid: true,
   },
   themes: ['@docusaurus/theme-mermaid'],
   themeConfig: {
-    algolia: {
-      // The application ID provided by Algolia
-      appId: 'YOUR_APP_ID',
 
-      // Public API key: it is safe to commit it
-      apiKey: 'YOUR_SEARCH_API_KEY',
-
-      indexName: 'YOUR_INDEX_NAME',
-
-      // Optional: see doc section below
-      contextualSearch: true,
-
-      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-      externalUrlRegex: 'external\\.com|domain\\.com',
-
-      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      replaceSearchResultPathname: {
-        from: '/docs/', // or as RegExp: /\/docs\//
-        to: '/',
-      },
-
-      // Optional: Algolia search parameters
-      searchParameters: {},
-
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
-
-      //... other Algolia params
-    },
     mermaid: {
       theme: {light: 'neutral', dark: 'dark'},
     },
