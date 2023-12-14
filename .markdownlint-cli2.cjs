a2cc37c36c46a770ba40d7a56f76246b6c9b117f
travisladuke <travisladuke@gmail.com>
2023-12-14 14:16:34 -0800

    modified:   .markdownlint-cli2.cjs

--- .markdownlint-cli2.cjs
+++ .markdownlint-cli2.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  "config": {
-    "html": false,
+  config: {
+    html: false,
     "line-length": false,
-  }
+    MD046: { style: "fenced" },
+    MD024: { allow_different_nesting: true },
+  },
 };
